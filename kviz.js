19394840957bbcc0e031ee6d498c50e92aeb5914
Petra Bočková <Wickky@seznam.cz>
2022-04-02 15:17:33 +0200

    finished added more 1 question + functional score

--- kviz.js
+++ kviz.js
@@ -2,11 +2,21 @@
 
 
 let poleOtazek = [
-    { poradi: 'Otázka 1/3', obrazek: 'obrazky/moncicak.jpg', otazka: 'Co je ikonická hračka z 80.let?', odpoved0: 'Kočičák', odpoved1: 'Mončičák', odpoved2: 'Opičák', odpoved3: '', zvolenaOdpoved: '', spravnaOdpoved: 'Mončičák' },
-    { poradi: 'Otázka 2/3', obrazek: 'obrazky/ovoce.jpg', otazka: 'Jaké je Matějovo nejoblíbenější ovoce?', odpoved0: 'Kokos', odpoved1: 'Meloun', odpoved2: 'Jahoda', odpoved3: 'Ani jedno z toho', zvolenaOdpoved: '', spravnaOdpoved: 'Jahoda' },
-    { poradi: 'Otázka 3/3', obrazek: 'obrazky/pivo.jpg', otazka: 'Co je potřeba pro úspěšné absolvování kurzu?', odpoved0: 'Umět JavaScript', odpoved1: 'Chodit po kurzu na pivo', odpoved2: '', odpoved3: '', zvolenaOdpoved: '', spravnaOdpoved: 'Umět JavaScript' }
+    { poradi: 'Otázka 1/4', obrazek: 'obrazky/azory.jpg', otazka: 'Jak se říká Azorským osrovům?', odpoved0: 'Ostrov zeleného zlata', odpoved1: 'Havaj středního Atlantiku', odpoved2: 'Perla Indického oceánu', odpoved3: '', spravnaOdpoved: 'Havaj středního Atlantiku' },
+
+
+
+    { poradi: 'Otázka 2/4', obrazek: 'obrazky/syr.jpg', otazka: 'Jak se jmenuje typický sýr pocházející z Azor?', odpoved0: 'Queijo de São Jorge', odpoved1: 'Comté', odpoved2: 'Mozzarella', odpoved3: 'Téte de Moinee', spravnaOdpoved: 'Queijo de São Jorg' },
+
+    { poradi: 'Otázka 3/4', obrazek: 'obrazky/zazitek.jpg', otazka: 'Co můžete zažít na Azorech?', odpoved0: 'pozorování velryb', odpoved1: 'jízda na velbloudovi', odpoved2: 'pštrosí závody', odpoved3: '', spravnaOdpoved: 'pozorování velryb' },
+
+    { poradi: 'Otázka 4/4', obrazek: 'obrazky/azory_ostrovy.jpg', otazka: 'Z kolika ostrovů se skládá souostroví Azor?', odpoved0: '6', odpoved1: '9', odpoved2: '', odpoved3: '', spravnaOdpoved: '9' }
+
 
 ];
+
+let poleOdpovedi = [];
+let pocetSpravne = [];
 
 
 
@@ -50,36 +60,45 @@ function zvoleniMoznosti(event) {
 
     console.log(event.srcElement.id);
 
-    if (event.srcElement.id == 0) {
-        poleOtazek[i].zvolenaOdpoved = odpoved0;
+
+    if (event.srcElement.id == '0') {
+        poleOdpovedi.push(poleOtazek[i].odpoved0);
+    }
+    else if (event.srcElement.id == '1') {
+        poleOdpovedi.push(poleOtazek[i].odpoved1);
+    }
+    else if (event.srcElement.id == '2') {
+        poleOdpovedi.push(poleOtazek[i].odpoved2);
+    }
+    else if (event.srcElement.id == '3') {
+        poleOdpovedi.push(poleOtazek[i].odpoved3);
     }
 
-    else if (event.srcElement.id == 1) {
-        poleOtazek[i].zvolenaOdpoved = odpoved1;
-    }
 
-    else if (event.srcElement.id == 2) {
-        poleOtazek[i].zvolenaOdpoved = odpoved2;
+    if (poleOdpovedi[i] == poleOtazek[i].spravnaOdpoved) {
+        pocetSpravne.push('1');
     }
-
-    else {
-        poleOtazek[i].zvolenaOdpoved = odpoved3;
-    }
-
 
     i = i + 1;
 
 
 
 
-    odpoved3.style.display = 'initial';
-    if (i < 3) {
+    odpoved3.style.display = 'inherit';
+    if (i < poleOtazek.length) {
 
         poradi.innerHTML = poleOtazek[i].poradi;
         obrazek.src = poleOtazek[i].obrazek;
         otazka.innerHTML = poleOtazek[i].otazka;
         odpoved0.innerHTML = poleOtazek[i].odpoved0;
         odpoved1.innerHTML = poleOtazek[i].odpoved1;
+        if ((poleOtazek[i].odpoved2) == '') {
+            odpovedi.removeChild(odpoved2);
+        }
+        else {
+            odpoved2.innerHTML = poleOtazek[i].odpoved2;
+        }
+
 
         if ((poleOtazek[i].odpoved3) == '') {
             odpovedi.removeChild(odpoved3);
@@ -88,12 +107,7 @@ function zvoleniMoznosti(event) {
             odpoved3.innerHTML = poleOtazek[i].odpoved3;
         }
 
-        if ((poleOtazek[i].odpoved2) == '') {
-            odpovedi.removeChild(odpoved2);
-        }
-        else {
-            odpoved2.innerHTML = poleOtazek[i].odpoved2;
-        }
+
 
 
     }
@@ -108,25 +122,13 @@ function zvoleniMoznosti(event) {
         vysledek.classList.toggle('vysledek');
 
         nadpisVysledek.innerHTML = ('Tvoje hodnocení');
-        textVysledek.innerHTML = `<h3>1. ${poleOtazek[0].otazka}</h3> Tvoje odpověď: ${poleOtazek[0].zvolenaOdpoved} <br> Správná odpověď: ${poleOtazek[0].spravnaOdpoved} <h3>2. ${poleOtazek[1].otazka}</h3> Tvoje odpověď:${poleOtazek[1].zvolenaOdpoved} <br> Správná odpověď: ${poleOtazek[1].spravnaOdpoved}
-        <h3>3. ${poleOtazek[2].otazka}</h3> Tvoje odpověď:${poleOtazek[2].zvolenaOdpoved} <br> Správná odpověď: ${poleOtazek[2].spravnaOdpoved} <br> <br>
+        textVysledek.innerHTML = `<h3>1. ${poleOtazek[0].otazka}</h3> Tvoje odpověď:  ${poleOdpovedi[0]} <br> Správná odpověď: ${poleOtazek[0].spravnaOdpoved} <h3>2. ${poleOtazek[1].otazka}</h3> Tvoje odpověď:  ${poleOdpovedi[1]} <br> Správná odpověď: ${poleOtazek[1].spravnaOdpoved}
+        <h3>3. ${poleOtazek[2].otazka}</h3> Tvoje odpověď:  ${poleOdpovedi[2]} <br> Správná odpověď: ${poleOtazek[2].spravnaOdpoved} <h3>1. ${poleOtazek[3].otazka}</h3> Tvoje odpověď:  ${poleOdpovedi[3]} <br> Správná odpověď: ${poleOtazek[3].spravnaOdpoved} <br> <br>
         `;
 
-        let spravne = '';
-        let uspesnost = '';
+        let uspesnost = (Math.floor((pocetSpravne.length * 100) / poleOtazek.length));
 
-        if ((poleOtazek[0].zvolenaOdpoved = poleOtazek[0].spravnaOdpoved) && (poleOtazek[1].zvolenaOdpoved = poleOtazek[1].spravnaOdpoved) && (poleOtazek[2].zvolenaOdpoved = poleOtazek[2].spravnaOdpoved)) {
-            spravne = '3';
-            uspesnost = '100%';
-
-        }
-
-        // přidat else if se bude rovnat aspoň jedna a pak když se nebude rovnat žádná a nebo aspoň dvě
-
-
-
-
-        hodnoceni.innerHTML = ('Správně ' + spravne + ' ze 3 otázek, úspěšnost je ' + uspesnost);
+        hodnoceni.innerHTML = ('Správně: ' + pocetSpravne.length + ' ze ' + poleOtazek.length + '  otázek, úspěšnost je ' + uspesnost + ' %');
 
 
     }
