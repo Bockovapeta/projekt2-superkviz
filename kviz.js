02be7ab3e42455ea316857f69187c34c808b73b0
Petra Bočková <Wickky@seznam.cz>
2022-04-02 10:48:01 +0200

    added nefungující  hodnocení

--- kviz.js
+++ kviz.js
@@ -2,9 +2,9 @@
 
 
 let poleOtazek = [
-    { poradi: 'Otázka 1/3', obrazek: 'obrazky/moncicak.jpg', otazka: 'Co je ikonická hračka z 80.let?', odpoved0: 'Kočičák', odpoved1: 'Mončičák', odpoved2: 'Opičák', odpoved3: '', odpoved: 'Mončičák' },
-    { poradi: 'Otázka 2/3', obrazek: 'obrazky/ovoce.jpg', otazka: 'Jaké je Matějovo nejoblíbenější ovoce?', odpoved0: 'Kokos', odpoved1: 'Meloun', odpoved2: 'Jahoda', odpoved3: 'Ani jedno z toho', odpoved: 'Jahoda' },
-    { poradi: 'Otázka 3/3', obrazek: 'obrazky/pivo.jpg', otazka: 'Co je potřeba pro úspěšné absolvování kurzu?', odpoved0: 'Umět JavaScript', odpoved1: 'Chodit po kurzu na pivo', odpoved2: '', odpoved3: '', odpoved: 'Umět JavaScript' }
+    { poradi: 'Otázka 1/3', obrazek: 'obrazky/moncicak.jpg', otazka: 'Co je ikonická hračka z 80.let?', odpoved0: 'Kočičák', odpoved1: 'Mončičák', odpoved2: 'Opičák', odpoved3: '', zvolenaOdpoved: '', spravnaOdpoved: 'Mončičák' },
+    { poradi: 'Otázka 2/3', obrazek: 'obrazky/ovoce.jpg', otazka: 'Jaké je Matějovo nejoblíbenější ovoce?', odpoved0: 'Kokos', odpoved1: 'Meloun', odpoved2: 'Jahoda', odpoved3: 'Ani jedno z toho', zvolenaOdpoved: '', spravnaOdpoved: 'Jahoda' },
+    { poradi: 'Otázka 3/3', obrazek: 'obrazky/pivo.jpg', otazka: 'Co je potřeba pro úspěšné absolvování kurzu?', odpoved0: 'Umět JavaScript', odpoved1: 'Chodit po kurzu na pivo', odpoved2: '', odpoved3: '', zvolenaOdpoved: '', spravnaOdpoved: 'Umět JavaScript' }
 
 ];
 
@@ -17,9 +17,7 @@ let odpoved0 = document.querySelector('li[data-odpoved="0"]');
 let odpoved1 = document.querySelector('li[data-odpoved="1"]');
 let odpoved2 = document.querySelector('li[data-odpoved="2"]');
 let odpoved3 = document.querySelector('li[data-odpoved="3"]');
-let zvolenaOdpoved1 = '';
-let zvolenaOdpoved2 = '';
-let zvolenaOdpoved3 = '';
+
 
 
 let odpovedi = document.querySelector('#odpovedi');
@@ -27,7 +25,10 @@ let kviz = document.querySelector('#kviz');
 let moznosti = document.querySelector('#moznosti');
 let obsah = document.querySelector('#obsah');
 let vysledek = document.querySelector('#vysledek');
-let nadpisVysledek = document.querySelector('.vysledek h2');
+let nadpisVysledek = document.querySelector('#vysledek h2');
+let textVysledek = document.querySelector('#vysledek p')
+let hodnoceni = document.querySelector('#hodnoceni h2');
+
 let i = 0;
 
 
@@ -49,7 +50,28 @@ function zvoleniMoznosti(event) {
 
     console.log(event.srcElement.id);
 
+    if (event.srcElement.id == 0) {
+        poleOtazek[i].zvolenaOdpoved = odpoved0;
+    }
+
+    else if (event.srcElement.id == 1) {
+        poleOtazek[i].zvolenaOdpoved = odpoved1;
+    }
+
+    else if (event.srcElement.id == 2) {
+        poleOtazek[i].zvolenaOdpoved = odpoved2;
+    }
+
+    else {
+        poleOtazek[i].zvolenaOdpoved = odpoved3;
+    }
+
+
     i = i + 1;
+
+
+
+
     odpoved3.style.display = 'initial';
     if (i < 3) {
 
@@ -80,11 +102,32 @@ function zvoleniMoznosti(event) {
 
     else {
 
-        /*
-                kviz.innerHTML = `<h2> Tvoje hodnocení </h2><h3>1. ${poleOtazek[0].otazka}</h3> Tvoje odpověď: ${poleOtazek[1].odpoved} <br> Správná odpověď: ${poleOtazek[0].odpoved} <h3>2. ${poleOtazek[1].otazka}</h3> Tvoje odpověď:${poleOtazek[0].otazka} <br> Správná odpověď: ${poleOtazek[1].odpoved}
-                <h3>3. ${poleOtazek[2].otazka}</h3> Tvoje odpověď:${poleOtazek[0].otazka} <br> Správná odpověď: ${poleOtazek[2].odpoved} <br> <br>
-                <h2> Správně 1 ze 3 otázek, úspěšnost je 33% </h2>`;
-        */
+        poradi.style.display = 'none';
+        otazka.style.display = 'none';
+        obsah.style.display = 'none';
+        vysledek.classList.toggle('vysledek');
+
+        nadpisVysledek.innerHTML = ('Tvoje hodnocení');
+        textVysledek.innerHTML = `<h3>1. ${poleOtazek[0].otazka}</h3> Tvoje odpověď: ${poleOtazek[0].zvolenaOdpoved} <br> Správná odpověď: ${poleOtazek[0].spravnaOdpoved} <h3>2. ${poleOtazek[1].otazka}</h3> Tvoje odpověď:${poleOtazek[1].zvolenaOdpoved} <br> Správná odpověď: ${poleOtazek[1].spravnaOdpoved}
+        <h3>3. ${poleOtazek[2].otazka}</h3> Tvoje odpověď:${poleOtazek[2].zvolenaOdpoved} <br> Správná odpověď: ${poleOtazek[2].spravnaOdpoved} <br> <br>
+        `;
+
+        let spravne = '';
+        let uspesnost = '';
+
+        if ((poleOtazek[0].zvolenaOdpoved = poleOtazek[0].spravnaOdpoved) && (poleOtazek[1].zvolenaOdpoved = poleOtazek[1].spravnaOdpoved) && (poleOtazek[2].zvolenaOdpoved = poleOtazek[2].spravnaOdpoved)) {
+            spravne = '3';
+            uspesnost = '100%';
+
+        }
+
+        // přidat else if se bude rovnat aspoň jedna a pak když se nebude rovnat žádná a nebo aspoň dvě
+
+
+
+
+        hodnoceni.innerHTML = ('Správně ' + spravne + ' ze 3 otázek, úspěšnost je ' + uspesnost);
+
 
     }
 }
